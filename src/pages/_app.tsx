7c79b5d92dea314c331237e724530a4436755919
yuyake0084 <yuyake0084@gmail.com>
2022-10-17 20:44:33 +0900

    large-small-dynamic-viewport-units-polyfillを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "~/styles/reset.css";
 import "~/styles/globals.css";
+import "large-small-dynamic-viewport-units-polyfill";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
